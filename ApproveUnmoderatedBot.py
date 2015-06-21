03b3e8f0ee0251cadaa3596d36ec2a418395b706
x_minus_one <xminusone@users.noreply.github.com>
2015-06-21 00:44:02 -0400

    cleaned up code, added timestamp each cycle

--- ApproveUnmoderatedBot.py
+++ ApproveUnmoderatedBot.py
@@ -2,45 +2,50 @@ import praw
 import datetime
 import time
 
-print('/r/NotTheOnion Unmoderated Approval Bot - v1.0b')
-print('Logging in to Reddit...')
-r = praw.Reddit('NTO Unmoderated Approvals v1.0b - /u/you')
+print('Dead Post Approval Bot - v1.0b')
+print('Logging in to reddit account...')
+r = praw.Reddit('Dead Post Approvals v1.0 - /u/YOURUSERNAME')
 r.login('USERNAME', 'PASSWORD')
+print('Success!')
 
-# time goes here if doesn't work below
+# Print Current Timestamp, stolen from titlecheckbot
+def printCurrentTime():
+    currentSysTime = time.localtime()
+    print(time.strftime('(%m/%d/%y: %H:%M:%S)', currentSysTime))
 
+# Actual bot stuff
 def queueCheckerBot():
+    print('Starting cycle.')
+    printCurrentTime()
     print('Updating current time...')
     now = datetime.datetime.now(datetime.timezone.utc).timestamp()
-    print('Grabbing unmoderated for /r/mod...')
+    print('Grabbing unmoderated queue for /r/mod...')
     s = r.get_subreddit('mod')
-    unmoderated = s.get_unmoderated(limit=1000)
-    print('Checking all unmoderated submissions...')
+    unmoderated = s.get_unmoderated(limit=1000) # Change this limit if you want to have it check less submissions for some reason
+    print('Checking age of unmoderated submissions.')
     for submission in unmoderated:
         try:
             age = now - submission.created_utc
             if age < 86400:
+                # Skips posts under 24hrs old
                 print('Submission is less than 24hrs old, continuing.')
-                # time.sleep(1)
                 continue
             if len(submission.mod_reports + submission.user_reports) > 0:
+                # Skips posts that have reports
                 print('Submission has reports, continuing.')
-                # time.sleep(1)
                 continue
             print('Submission is 24hrs old, approving...')
-            # time.sleep(1)
+            # Approves posts that are 24hrs old
             submission.approve()
             print('Done!')
-        # print('All submissions checked. Exiting in 60 seconds...')
-        # time.sleep(60)
-        # sys.exit(0)
         except:
-            print('Error! Reddit gave us an error. Skipping.')
+            print('Error! Skipping submission.')
 while True:
     try:
         queueCheckerBot()
-        print('Checked all submissions. Rechecking in 60min.')
+        print('Cycle complete! Checked all submissions. Rechecking in 60min.')
+        print('----------')
         time.sleep(3600)
     except:
-        print('Error! Something broke. Retrying.')
+        print('Error! Something broke. Retrying cycle.')
         pass
